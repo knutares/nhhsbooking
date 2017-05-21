a2ddb161cde6b7fa63e6f815bbe7f31cb75b0ff0
faeried <kim_k1412@hotmail.com>
2017-05-21 09:48:35 +0200

    34rt34t545y

--- app.py
+++ app.py
@@ -1,43 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('json.html')
-"""
-@app.route('/bekreftelse')
-def index():
-    return render_template('mb_bekreftelsesside.html')
-
-@app.route('/skjema')
-def index():
-    return render_template('mb_utfyllingskjema.html')
-
-@app.route('/moterombooking')
-def index():
-    return render_template('moterombooking.html')
 
 @app.route('/velgtid')
-def index():
-    return render_template('velgtidrom.html')
+def velgtid():
+    return render_template('velgtid.html')
 
-@app.route('/logginn')
-def medlemsliste():
-    return render_template('loginn.html')
+#
+# @app.route('/bekreftelse')
+# def bekrefetelse():
+#     return render_template('mb_bekreftelsesside.html')
+#
+# @app.route('/skjema')
+# def skjema():
+#     return render_template('mb_utfyllingskjema.html')
+#
+# @app.route('/moterombooking')
+# def møtebooking():
+#     return render_template('moterombooking.html')
+#
+# @app.route('/loginn')
+# def loginn():
+#     return render_template('loginn.html')
+#
+# @app.route('/data')
+# def return_data():
+#     start_date = request.args.get('start', '')
+#     end_date = request.args.get('end', '')
+#
+#     with open("events.json", "r") as input_data:
+#         return input_data.read()
 
-@app.route('/logginn')
-def medlemsliste():
-    return render_template('kalender.html')
-"""
-
-@app.route('/data')
-def return_data():
-    start_date = request.args.get('start', '')
-    end_date = request.args.get('end', '')
-
-    with open("events.json", "r") as input_data:
-        return input_data.read()
 
 if __name__ == '__main__':
     app.run()

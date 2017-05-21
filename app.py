b08ef7149a6784ac54a3c00b47deeeabd2e45920
faeried <kim_k1412@hotmail.com>
2017-05-21 08:19:04 +0200

    34rt34t545y

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, request
-import sqlite3
 
 app = Flask(__name__)
 
-def connect_db():
-    return sqlite3.connect('app.db')
-
 @app.route('/')
 def index():
-    return render_template('startside.html')
-
+    return render_template('json.html')
+"""
 @app.route('/bekreftelse')
 def index():
     return render_template('mb_bekreftelsesside.html')
@@ -29,6 +25,19 @@ def index():
 @app.route('/logginn')
 def medlemsliste():
     return render_template('loginn.html')
+
+@app.route('/logginn')
+def medlemsliste():
+    return render_template('kalender.html')
+"""
+
+@app.route('/data')
+def return_data():
+    start_date = request.args.get('start', '')
+    end_date = request.args.get('end', '')
+
+    with open("events.json", "r") as input_data:
+        return input_data.read()
 
 if __name__ == '__main__':
     app.run()

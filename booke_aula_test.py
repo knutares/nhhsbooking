b95cb974f1040e8942ae8347c657bc9da5379fba
Faeried <kim_k1412@hotmail.com>
2017-09-24 13:12:03 +0200

    isaktester
    fsgbsrb

---
+++ booke_aula_test.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    A = {kku.bar@nhhs.no:""}
+    if test
+    return render_template('json.html')
+
+if __name__ == '__main__':
+    app.run()
